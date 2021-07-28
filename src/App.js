28b59840926ae4db5825707004663ee3a031db02
Nicholas Ochs <nick.ochs@yahoo.com>
2021-07-28 18:49:10 -0400

    creates theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { createTheme, ThemeProvider } from '@material-ui/core';
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#C41D33'
+      },
+      secondary: {
+        main: '#FFF'
+      },
+    }
+  });
+
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+      </>
+    </ThemeProvider>
   );
 }
 
