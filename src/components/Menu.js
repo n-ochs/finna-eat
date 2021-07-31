442fbfe75bdf60f99bb5caaf2b9ca55835331fc7
n-ochs <nick.ochs@yahoo.com>
2021-07-31 19:16:01 -0400

    adds menu layout

---
+++ src/components/Menu.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Container } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginTop: '1%'
+    },
+    container: {
+        height: '100%',
+        width: 'auto',
+        border: 'solid black'
+    }
+}));
+
+function Menu() {
+    const classes = useStyles();
+
+    return (
+        <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+                <Grid container justifyContent='center' spacing={10}>
+                    {[0, 1].map((value) => (
+                        <Grid key={value} item>
+                            <Container className={classes.container}>
+                                Menu Item
+                            </Container>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Menu;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Menu from '../components/Menu';
 
 function Home() {
     return (
+        <>
         <Hero />
+        <Menu />
+        </>
     );
 };
 
