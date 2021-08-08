191a17fffca7d02a2a2a74d9366c25c125ad9aad
Andrew Bell <andrewbell@Andrews-Mac-mini.local>
2021-08-08 12:28:06 -0400

    added map

--- src/pages/Find.js
+++ src/pages/Find.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../App.css';
+import GoogleMap from '../components/GoogleMap';
+
 
 function Find() {
     return (
-        <>
-        </>
+        <div className="App">
+        <h1>Finna Eat Location</h1>
+        <GoogleMap />
+        </div>
     );
 };
 
