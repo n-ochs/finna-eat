ddd735d03a3ffca5d32ed119fac4ef095bbccdeb
n-ochs <nick.ochs@yahoo.com>
2021-08-23 20:22:45 -0400

    adds unstyled menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Container } from '@material-ui/core';
+import { Grid, Container, Typography } from '@material-ui/core';
+import menu from '../data/menu.json';
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -11,26 +12,84 @@ const useStyles = makeStyles(() => ({
         height: '100%',
         width: 'auto',
         border: 'solid black'
+    },
+    itemName: {
+        fontStyle: 'italic'
     }
 }));
 
 function Menu() {
     const classes = useStyles();
+    console.log(menu.Lagniappe)
 
     return (
-        <Grid container className={classes.root} spacing={2}>
-            <Grid item xs={12}>
-                <Grid container justifyContent='center' spacing={10}>
-                    {[0, 1].map((value) => (
-                        <Grid key={value} item>
-                            <Container className={classes.container}>
-                                Menu Item
-                            </Container>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Grid>
-        </Grid>
+        <>
+
+        <Typography variant='h1'>
+            Menu
+        </Typography>
+        
+        <Typography variant='h5'>
+            Lagniappe
+        </Typography>
+
+        {menu.Lagniappe.map((item) => (
+            <>
+                <Typography key={item.Name} variant='subtitle1'>{item.Name}</Typography>
+                <Typography key={item.Name} className={classes.itemName} variant='caption'>{item.Description}</Typography>
+            </>
+        ))}
+
+        <Typography variant='h5'>
+            Vegetarian
+        </Typography>
+
+        {menu.Vegetarian.map((item) => (
+            <>
+                <Typography key={item.Name} variant='subtitle1'>{item.Name}</Typography>
+                <Typography key={item.Name} className={classes.itemName} variant='caption'>{item.Description}</Typography>
+            </>
+        ))}
+
+        <Typography variant='h5'>
+            Carnivore
+        </Typography>
+
+        {menu.Carnivore.map((item) => (
+            <>
+                <Typography key={item.Name} variant='subtitle1'>{item.Name}</Typography>
+                <Typography key={item.Name} className={classes.itemName} variant='caption'>{item.Description}</Typography>
+            </>
+        ))}
+
+        <Typography variant='h5'>
+            Pescatarian
+        </Typography>
+
+        {menu.Pescatarian.map((item) => (
+            <>
+                <Typography key={item.Name} variant='subtitle1'>{item.Name}</Typography>
+                <Typography key={item.Name} className={classes.itemName} variant='caption'>{item.Description}</Typography>
+            </>
+        ))}
+
+        </>
+
+        // <Grid container className={classes.root} spacing={2}>
+        //     <Grid item xs={12}>
+        //         <Grid container justifyContent='center' spacing={10}>
+        //             {menu.Lagniappe.map((item) => (
+        //                 <Grid key={item.Name} item>
+        //                     <Container className={classes.container}>
+        //                         <Typography>
+        //                             {item.Name}
+        //                         </Typography>
+        //                     </Container>
+        //                 </Grid>
+        //             ))}
+        //         </Grid>
+        //     </Grid>
+        // </Grid>
     );
 };
 
