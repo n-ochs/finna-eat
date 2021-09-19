2784d424be658c4d00f055b1a87f991c4ffdb73b
Nicholas Ochs <nick.ochs@yahoo.com>
2021-09-19 18:38:11 -0400

    adds divider and copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Box } from '@material-ui/core';
+import { makeStyles, Box, Typography } from '@material-ui/core';
 import { FaInstagram, FaFacebookSquare } from 'react-icons/fa';
 import { IoLogoTiktok } from 'react-icons/io5';
 
@@ -8,25 +8,52 @@ const useStyles = makeStyles(() => ({
         width: '100%',
         height: '300px',
         background: '#1e1415',
-        display: 'flex'
+        display: 'flex',
+        flexWrap: 'wrap'
     },
     content: {
+        height: 'auto',
+        // width: '400px',
         margin: 'auto',
         color: 'white',
     },
     socials: {
-
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '10%'
     },
     icons: {
         width: '35px',
         height: '35px',
         marginLeft: '5px',
         marginRight: '5px'
+    },
+    contact: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    phoneNumber: {
+        marginRight: '10%'
+    },
+    email: {
+        marginLeft: '10%'
+    },
+    divider: {
+        height: '0',
+        width: '100%',
+        border: 'solid white',
+        borderWidth: 'thin',
+        marginTop: '5%'
+    },
+    copyright: {
+        width: '100%',
+        textAlign: 'center',
+        color: 'white'
     }
 }));
 
 function Footer() {
-    const { root, content, socials, icons } = useStyles();
+    const { root, content, socials, icons, contact, phoneNumber, email, divider, copyright } = useStyles();
     return (
         <Box className={root}>
             <Box className={content}>
@@ -35,6 +62,20 @@ function Footer() {
                     <FaInstagram className={icons} />
                     <IoLogoTiktok className={icons} />
                 </Box>
+                <Box className={contact}>
+                    <Typography className={phoneNumber} display='inline' variant='body2' component='p'>
+                        123-123-1234
+                    </Typography>
+                    <Typography className={email} display='inline' variant='body2' component='p'>
+                        finnaeat@gmail.com
+                    </Typography>
+                </Box>
+            </Box>
+            <Box className={divider} />
+            <Box className={copyright}>
+                <Typography>
+                    &copy; 2021 Finna-Eat | All Rights Reserved
+                </Typography>
             </Box>
         </Box>
     );
