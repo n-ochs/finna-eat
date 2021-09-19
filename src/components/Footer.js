f5ae7d3162fa356f5cf44be928ab5faa19009d12
Nicholas Ochs <nick.ochs@yahoo.com>
2021-09-19 18:01:54 -0400

    adds social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles, Box } from '@material-ui/core';
+import { FaInstagram, FaFacebookSquare } from 'react-icons/fa';
+import { IoLogoTiktok } from 'react-icons/io5';
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -10,20 +12,28 @@ const useStyles = makeStyles(() => ({
     },
     content: {
         margin: 'auto',
-        color: 'white'
+        color: 'white',
     },
     socials: {
 
+    },
+    icons: {
+        width: '35px',
+        height: '35px',
+        marginLeft: '5px',
+        marginRight: '5px'
     }
-}))
+}));
 
 function Footer() {
-    const { root, content, socials } = useStyles();
+    const { root, content, socials, icons } = useStyles();
     return (
         <Box className={root}>
             <Box className={content}>
                 <Box className={socials}>
-
+                    <FaFacebookSquare className={icons} />
+                    <FaInstagram className={icons} />
+                    <IoLogoTiktok className={icons} />
                 </Box>
             </Box>
         </Box>
